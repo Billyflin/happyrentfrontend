0b7cbedfe8ad54da8ab9e3494c96dfb0c3f0e75b
Billyflin <billymartinezc@gmail.com>
2024-02-23 10:51:52 -0300

    sancando el mensaje de la mona china

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -12,7 +12,7 @@
       <div class="row gx-4">
         <div class="col-auto">
           <div class="avatar avatar-xl position-relative">
-            <img src="@/assets/img/Billyflin.jpg" alt="profile_image" class="shadow-sm w-100 border-radius-lg" />
+            <img src="@/assets/img/bruce-mars.jpg" alt="profile_image" class="shadow-sm w-100 border-radius-lg" />
           </div>
         </div>
         <div class="col-auto my-auto">
