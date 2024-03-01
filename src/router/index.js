7277b2d57f8de0d9d29a9a8e040784b8078b2933
Billyflin <billymartinezc@gmail.com>
2024-03-01 02:39:41 -0300

    Re estructura coso

--- src/router/index.js
+++ src/router/index.js
@@ -19,18 +19,11 @@ const routes = [{
     requiresAuth: true, roles: ['ROLE_USER']
   }
 }, {
-  path: '/addProduct', name: 'AddProduct', component: () => import('../views/AddProduct.vue'), meta: {
-    requiresAuth: true, roles: ['ROLE_ADMIN']
-  }
-}, {
   path: '/sign-in', name: 'SignIn', component: SignIn
 }, {
   path: '/sign-up', name: 'SignUp', component: SignUp
-}, {
-  path: '/contact', name: 'Contact', component: () => import('../views/Contact.vue'), meta: {
-    roles: ['ROLE_USER']
-  }
-}, {
+}
+, {
   path: '/misContratos', name: 'MisContratos', component: () => import('../views/Contrato/MisContratos.vue'), meta: {
     requiresAuth: true, roles: ['ROLE_USER']
   }
