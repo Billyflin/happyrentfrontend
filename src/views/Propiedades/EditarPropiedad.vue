ec37d46af77ed81f249538ef5e5fc27a9f762841
Dankusufro <86026664+Dankusufro@users.noreply.github.com>
2024-03-07 21:14:44 -0300

    editar propiedad

--- src/views/Propiedades/EditarPropiedad.vue
+++ src/views/Propiedades/EditarPropiedad.vue
@@ -1,17 +1,8 @@
-<script setup>
-
-import MaterialInput from '@/components/MaterialInput.vue'
-import DefaultProjectCard from '@/views/components/DefaultProjectCard/DefaultProjectCard.vue'
-</script>
 <template>
   <div class="py-4 container-fluid">
     <div class="row">
       <div class="col-lg-6">
-        <h4>Make the changes below</h4>
-        <p>
-          We’re constantly trying to express ourselves and actualize our dreams.
-          If you have the opportunity to play.
-        </p>
+        <h4>Edita la propiedad a continuación</h4>
       </div>
       <div
         class="text-right col-lg-6 d-flex flex-column justify-content-center"
@@ -19,252 +10,192 @@ import DefaultProjectCard from '@/views/components/DefaultProjectCard/DefaultPro
         <button
           type="button"
           class="mt-2 mb-0 btn bg-gradient-success ms-lg-auto me-lg-0 me-auto mt-lg-0"
+          @click="acciones.guardar"
         >
-          Save
+          Guardar
         </button>
       </div>
     </div>
-    <div class="row mt-4">
-      <div class="col-12">
-        <div class="mb-5 ps-3">
-          <h6 class="mb-1">Projects</h6>
-          <p class="text-sm">Architects design houses</p>
-        </div>
-        <div class="row">
-          <default-project-card
-            title="Modern"
-            :image="img1"
-            label="Project #2"
-            description="As Uber works through a huge amount of internal
-                management turmoil."
-            :authors="[
-                  {
-                    image: team1,
-                    name: 'Elena Morison',
-                  },
-                  {
-                    image: team2,
-                    name: 'Ryan Milly',
-                  },
-                  {
-                    image: team3,
-                    name: 'Nick Daniel',
-                  },
-                  {
-                    image: team4,
-                    name: 'Peterson',
-                  },
-                ]"
-            :action="{
-                  color: 'success',
-                  label: 'View Project',
-                }"
-          />
-
-          <default-project-card
-            title="Scandinavian"
-            :image="img2"
-            label="Project #1"
-            description="Music is something that every person has his or her own
-                      specific opinion about."
-            :authors="[
-                  {
-                    image: team3,
-                    name: 'Nick Daniel',
-                  },
-                  {
-                    image: team4,
-                    name: 'Peterson',
-                  },
-                  {
-                    image: team1,
-                    name: 'Elena Morison',
-                  },
-                  {
-                    image: team2,
-                    name: 'Ryan Milly',
-                  },
-                ]"
-            :action="{
-                  color: 'success',
-                  label: 'View Project',
-                }"
-          />
-
-          <default-project-card
-            title="Minimalist"
-            :image="img3"
-            label="Project #3"
-            description="Different people have different taste, and various types
-                      of music."
-            :authors="[
-                  {
-                    image: team4,
-                    name: 'Peterson',
-                  },
-                  {
-                    image: team3,
-                    name: 'Nick Daniel',
-                  },
-                  {
-                    image: team1,
-                    name: 'Elena Morison',
-                  },
-                  {
-                    image: team2,
-                    name: 'Ryan Milly',
-                  },
-                ]"
-            :action="{
-                  color: 'success',
-                  label: 'View Project',
-                }"
-          />
-          <default-project-card
-            title="Gothic"
-            image="https://images.unsplash.com/photo-1606744824163-985d376605aa?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"
-            label="Project #4"
-            description="Why would anyone pick blue over pink? Pink is obviously a
-                      better color."
-            :authors="[
-                  {
-                    image: team4,
-                    name: 'Peterson',
-                  },
-                  {
-                    image: team3,
-                    name: 'Nick Daniel',
-                  },
-                  {
-                    image: team2,
-                    name: 'Ryan Milly',
-                  },
-                  {
-                    image: team1,
-                    name: 'Elena Morison',
-                  },
-                ]"
-            :action="{
-                  color: 'success',
-                  label: 'View Project',
-                }"
-          />
-        </div>
-      </div>
-    </div>
     <div class="mt-4 row">
+<!--    imagen de la propiedad-->
       <div class="col-lg-4">
         <div class="card mt-4" data-animation="true">
-<!--          <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">-->
-<!--            <a class="d-block blur-shadow-image">-->
-<!--              <img-->
-<!--                src="@/assets/img/products/product-11.jpg"-->
-<!--                alt="img-blur-shadow"-->
-<!--                class="img-fluid shadow border-radius-lg"-->
-<!--              />-->
-<!--            </a>-->
-<!--            <div-->
-<!--              class="colored-shadow"-->
-<!--              style="-->
-<!--                background-image: url('@/assets/img/products/product-11.jpg');-->
-<!--              "-->
-<!--            ></div>-->
-<!--          </div>-->
           <div class="card-body text-center">
-            <div class="mt-n6 mx-auto">
+            <img
+              class="mt-3 rounded img-fluid"
+            src="https://images.unsplash.com/photo-1606744824163-985d376605aa?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"
+            alt="Propiedad"
+            >
+            <p class="mb-0">
+              El lugar está cerca de la playa de Barceloneta y la parada de autobús a solo 2 minutos a pie y cerca de "Naviglio" donde puedes disfrutar de la vida nocturna principal en Barcelona.
+            </p>
+            <div class="mt-3 mx-auto"> <!-- Modificamos el margen para que los botones estén debajo del texto -->
               <button
                 class="btn bg-gradient-success btn-sm mb-0 me-2"
                 type="button"
                 name="button"
+                @click="acciones.editar"
               >
-                Edit
+                Editar
               </button>
               <button
                 class="btn btn-outline-dark btn-sm mb-0"
                 type="button"
                 name="button"
+                @click="acciones.eliminar"
               >
-                Remove
+                Eliminar
               </button>
             </div>
-            <h5 class="font-weight-normal mt-4">Product Image</h5>
-            <p class="mb-0">
-              The place is close to Barceloneta Beach and bus stop just 2 min by
-              walk and near to "Naviglio" where you can enjoy the main night
-              life in Barcelona.
-            </p>
           </div>
         </div>
       </div>
+<!--    Información de la propiedad-->
       <div class="mt-4 col-lg-8 mt-lg-0">
         <div class="card">
           <div class="card-body">
-            <h5 class="font-weight-bolder">Product Information</h5>
-            <div class="row mt-4">
-              <div class="col-12 col-sm-6">
-                <material-input id="name" variant="dynamic" label="Name" />
-              </div>
-              <div class="mt-3 col-12 col-sm-6 mt-sm-0">
-                <material-input id="weight" variant="dynamic" label="Weight" />
-              </div>
-            </div>
-            <div class="row mt-4">
-              <div class="col-3">
-                <material-input
-                  id="collection"
-                  variant="dynamic"
-                  label="Collection"
-                />
-              </div>
-              <div class="col-3">
-                <material-input id="price" variant="dynamic" label="Price" />
-              </div>
-              <div class="col-3">
-                <material-input id="quantity" variant="dynamic" label="Quantity" />
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-sm-6">
-                <label class="mt-4 ms-0">Description</label>
-                <p class="text-xs form-text text-muted ms-1 d-inline">
-                  (optional)
-                </p>
-                <div id="edit-description-edit" class="h-50">
-                  Long sleeves black denim jacket with a twisted design.
-                  Contrast stitching. Button up closure. White arrow prints on
-                  the back.
+            <h5 class="font-weight-bolder">Información de la Propiedad</h5>
+            <div class="card-body pt-0">
+              <div class="row">
+                <div class="col-6">
+                  <material-input
+                    id="PropiedadName"
+                    variant="static"
+                    label="Nombre Propiedad"
+                    placeholder="Casa de campo, etc"
+                    v-model="propiedad.nombre"
+                  />
+                </div>
+                <div class="col-4">
+                  <material-choices id="tipo_propiedad"
+                                    :options="[
+                              {value:'Casa',text:'Casa'},
+                              {value:'Terreno',text:'Terreno'},
+                              {value:'Departamento',text:'Departamento'}
+                              ]"
+                                    name="TipoPropiedad" label="Tipo Propiedad" :search-enabled="false" />
                 </div>
               </div>
+              <div class="row">
+                <div class="col-3">
+                  <material-input
+                    id="numBanos"
+                    type="number"
+                    variant="static"
+                    label="baños"
+                    placeholder="1"
+                    v-model="propiedad.numBanos"
+                    @input="validateInput"
+                  />
+                </div>
+                <div class="col-3">
+                  <material-input
+                    id="numEstacionamientos"
+                    type="number"
+                    variant="static"
+                    label="estacionamientos"
+                    placeholder="1"
+                    v-model="propiedad.numEstacionamientos"
+                    @input="validateInput"
+                  />
+                </div>
+                <div class="col-3">
+                  <material-input
+                    id="numPiezas"
+                    type="number"
+                    variant="static"
+                    label="piezas"
+                    placeholder="1"
+                    v-model="propiedad.numPiezas"
+                    @input="validateInput"
+                  />
+                </div>
+                <div class="col-3">
+                  <material-input
+                    id="metrosCuadradosTerreno"
+                    type="number"
+                    variant="static"
+                    label="metros cuadrados terreno"
+                    placeholder="50"
+                    v-model="propiedad.metrosCuadradosTerreno"
+                    @input="validateInput"
+                  />
+                </div>
+                <div class="col-4">
+                  <material-input
+                    id="metrosCuadradosConstruidos"
+                    type="number"
+                    variant="static"
+                    label="metros cuadrados construidos"
+                    placeholder="40"
+                    v-model="propiedad.metrosCuadradosConstruidos"
+                    @input="validateInput"
+                  />
+                </div>
+                <div class="col-3">
+                  <material-input
+                    id="numBodegas"
+                    type="number"
+                    variant="static"
+                    label="numero de bodegas"
+                    placeholder="1"
+                    v-model="propiedad.numBodegas"
+                    @input="validateInput"
+                  />
+                </div>
+              </div>
+            <div class="row">
               <div class="col-sm-6">
-                <label class="mt-4 ms-0">Category</label>
-                <select
-                  id="choices-category-edit"
-                  class="form-control"
-                  name="choices-category"
-                >
-                  <option value="Choice 1" selected>Furniture</option>
-                  <option value="Choice 2">Real Estate</option>
-                  <option value="Choice 3">Electronics</option>
-                  <option value="Choice 4">Clothing</option>
-                  <option value="Choice 5">Others</option>
-                </select>
-                <label class="mt-3 ms-0">Color</label>
-                <select
-                  id="choices-color-edit"
-                  class="form-control"
-                  name="choices-color"
-                >
-                  <option value="Choice 1" selected>Black</option>
-                  <option value="Choice 2">White</option>
-                  <option value="Choice 3">Blue</option>
-                  <option value="Choice 4">Orange</option>
-                  <option value="Choice 5">Green</option>
-                </select>
+                <label class="mt-4 ms-0">Descripción</label>
+                <p class="text-xs form-text text-muted ms-1 d-inline">
+                  (opcional)
+                </p>
+                <textarea class="form-control mt-1" rows="3"></textarea>
               </div>
             </div>
           </div>
+            </div>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<script setup>
+import MaterialInput from '@/components/MaterialInput.vue'
+import MaterialChoices from '@/components/MaterialChoices.vue'
+import { defineEmits, ref, watchEffect } from 'vue'
+
+const propiedad = ref({
+  nombre: 'casa de campo',
+  tipo: 'casa',
+  numBanos: 0,
+  numEstacionamientos: 0,
+  numPiezas: 0,
+  metrosCuadradosTerreno: 0,
+  metrosCuadradosConstruidos: 0,
+  numBodegas: 0,
+  imagenes: []
+})
+const validateInput = (event) => {
+  if (event.target.value < 0) {
+    event.target.value = 0
+  }
+}
+const emit = defineEmits(['update:propiedad'])
+watchEffect(() => {
+  emit('update:propiedad', propiedad.value)
+})
+
+const acciones = {
+  guardar: () => {
+    console.log(propiedad.value)
+  },
+  editar: () => {
+    console.log('editar')
+  },
+  eliminar: () => {
+    console.log('eliminar')
+  }
+}
+</script>
