3ecd6c69b6e53e5b86e5bda594a78e97c2f28589
Billy <Billymartinezc@gmail.com>
2024-05-10 22:55:43 -0400

    Dashboard.vue

--- src/examples/Charts/LineChart.vue
+++ src/examples/Charts/LineChart.vue
@@ -5,45 +5,43 @@
 </template>
 
 <script setup>
-import {onMounted} from 'vue'
+import { onMounted } from 'vue'
 import * as echarts from 'echarts'
 
 const props = defineProps({
   id: {
     type: String,
-    default: 'line-chart',
+    default: 'line-chart'
   },
   height: {
     type: [Number, String],
-    default: '170',
+    default: '170'
   },
   yAxisMin: {
     type: Number,
-    default: 0,
+    default: 0
   },
   yAxisMax: {
     type: Number,
-    default: null,
+    default: null
   },
-  chart: {
+  data: {
     type: Object,
-    required: true,
-    xAxislDatas: Array,
-    datasets: {
-      label: String,
-      data: Array,
-    },
-  },
+    required: true
+  }
 })
 
 onMounted(() => {
+  console.log(props.data)
   const chartDom = document.getElementById(props.id)
   const echart = echarts.init(chartDom)
   window.addEventListener('resize', () => setTimeout(echart.resize, 200))
 
-  const xAxislDatas = props.chart.xAxislDatas
-  const data = props.chart.datasets.data
-  const label = props.chart.datasets.label
+  const xAxislDatas = props.data.map(obs => obs.indexDateString)
+  const chartData = props.data.map(obs => parseFloat(obs.value))
+  console.log(xAxislDatas)
+  console.log(chartData)
+
 
   const option = {
     grid: {
@@ -51,28 +49,28 @@ onMounted(() => {
       right: '20',
       bottom: '8%',
       top: '12%',
-      containLabel: true,
+      containLabel: true
     },
     tooltip: {
       trigger: 'axis',
       backgroundColor: 'rgba(0, 0, 0, 0.8)',
       textStyle: {
-        color: '#fff',
+        color: '#fff'
       },
       borderWidth: 0,
       axisPointer: {
-        type: 'none',
-      },
+        type: 'none'
+      }
     },
     xAxis: {
       type: 'category',
       data: xAxislDatas,
       boundaryGap: false, // 坐标轴两边留白策略
       axisLine: {
-        show: false, // 隐藏X轴线
+        show: false // 隐藏X轴线
       },
       axisTick: {
-        show: false, // 隐藏X轴刻度线
+        show: false // 隐藏X轴刻度线
       },
       axisLabel: {
         color: '#f8f9fa',
@@ -81,24 +79,24 @@ onMounted(() => {
         fontSize: 14,
         fontWeight: 300,
         fontFamily: 'Roboto',
-        lineHeight: 2,
+        lineHeight: 2
       },
       splitLine: {
         show: false, // 显示X轴网格线
         lineStyle: {
           color: 'rgba(255, 255, 255, .2)',
-          type: 'dashed',
-        },
-      },
+          type: 'dashed'
+        }
+      }
     },
     yAxis: {
       type: 'value',
       splitNumber: 3, // Y轴分段数
       axisLine: {
-        show: false, // 隐藏Y轴线
+        show: false // 隐藏Y轴线
       },
       axisTick: {
-        show: false, // 隐藏Y轴刻度线
+        show: false // 隐藏Y轴刻度线
       },
       axisLabel: {
         color: '#fff',
@@ -106,42 +104,43 @@ onMounted(() => {
         fontSize: 14,
         fontWeight: 300,
         fontFamily: 'Roboto',
-        lineHeight: 2,
+        lineHeight: 2
       },
       splitLine: {
         show: true, // 显示Y轴网格线
         lineStyle: {
           color: 'rgba(255, 255, 255, .4)',
-          type: 'dashed',
-        },
+          type: 'dashed'
+        }
       },
       min: props.yAxisMin,
-      max: props.yAxisMax !== null ? props.yAxisMax : Math.max(...data) < 100 ? Math.max(...data) + 10 : (Math.ceil(Math.max(...data) / 100) + 1) * 100,
+      max: props.yAxisMax !== null ? props.yAxisMax : Math.max(...chartData) < 100 ? Math.max(...chartData) + 10 : (Math.ceil(Math.max(...chartData) / 100) + 1) * 100
+
     },
     series: [
       {
-        name: label,
-        data: data,
+        name: 'valor',
+        data: chartData,
         type: 'line',
         barMaxWidth: 6,
         symbolSize: 10,
         itemStyle: {
           color: 'rgba(255, 255, 255)',
-          opacity: 0.8,
+          opacity: 0.8
         },
         lineStyle: {
           color: 'rgba(255, 255, 255)',
           opacity: 0.8,
-          width: 4,
+          width: 4
         },
         areaStyle: {
-          color: 'transparent',
+          color: 'transparent'
         },
         emphasis: {
-          scale: 0.8, // 鼠标移动到数据点时，将数据点缩小为初始大小的90%
-        },
-      },
-    ],
+          scale: 0.8 // 鼠标移动到数据点时，将数据点缩小为初始大小的90%
+        }
+      }
+    ]
   }
 
   option && echart.setOption(option)
@@ -153,3 +152,4 @@ onMounted(() => {
   width: 100%;
 }
 </style>
+
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,62 +2,61 @@
   <div class="py-4 container-fluid">
     <div class="row mb-4">
       <div class="col-lg-12 position-relative z-index-2">
-        <div class="row">
-          <mini-statistics-card
-              v-for="{title, detail, icon} of MiniStatisticsData"
-              :key="title.text"
-              :title="title"
-              :detail="detail"
-              :icon="icon"
-          />
-        </div>
         <div class="row mt-4">
-          <div class="col-lg-6 col-md-6 mt-4">
-            <ChartHolderCard v-if="DolarData"
+          <div class="col-lg-6 col-md-6 mt-6">
+            <ChartHolderCard v-if="DOLARData"
                              title="Valor del Dólar"
                              subtitle="Últimos valores"
                              :update="getMinutesSinceLastUpdate() > 0 ? `${getMinutesSinceLastUpdate()} minutos` : 'Actualizado'"
-                             color="happDark"
+                             color="success"
             >
-              <LineChart :chart="DolarData" id="line-chart-1" :yAxisMin="Math.min(...DolarData.datasets.data)"
-                         :y-axis-max="Math.max(...DolarData.datasets.data) "/>
+              <LineChart :data="DOLARData" id="line-chart-dolar" :yAxisMin="Math.min(...DOLARData.map(obs => parseFloat(obs.value)))"
+                         :y-axis-max="Math.max(...DOLARData.map(obs => parseFloat(obs.value)))" />
             </ChartHolderCard>
           </div>
-          <!--          modifica este grafico para ponerle uf-->
-
-          <div class="col-lg-6 mt-4">
-            <ChartHolderCard v-if="UFData"
-                             title="Valor de la Unidad de Fomento (UF)"
-                             subtitle="Last Campaign Performance"
+          <div class="col-lg-6 col-md-6 mt-6">
+            <ChartHolderCard v-if="UTMData"
+                             title="Valor del UTM"
+                             subtitle="Últimos valores"
                              :update="getMinutesSinceLastUpdate() > 0 ? `${getMinutesSinceLastUpdate()} minutos` : 'Actualizado'"
-                             color="dark"
+                             color="happLight"
             >
-              <LineChart :chart="UFData" id="line-chart-2" :yAxisMin="Math.min(...UFData.datasets.data)"
-                         :y-axis-max="Math.max(...UFData.datasets.data)"/>
+              <LineChart :data="UTMData" id="line-chart-3" :yAxisMin="Math.min(...UTMData.map(obs => parseFloat(obs.value)))"
+                         :y-axis-max="Math.max(...UTMData.map(obs => parseFloat(obs.value)))" />
+
             </ChartHolderCard>
           </div>
-        </div>
-        <div class="row mt-4">
-          <div class="col-lg-6 col-md-6 mt-4">
+          <div class="col-lg-6 col-md-6 mt-6">
+            <ChartHolderCard v-if="UFData"
+                             title="Valor del UF"
+                             subtitle="Últimos valores"
+                             :update="getMinutesSinceLastUpdate() > 0 ? `${getMinutesSinceLastUpdate()} minutos` : 'Actualizado'"
+                             color="primary"
+            >
+              <LineChart :data="UFData" id="line-chart-4" :yAxisMin="Math.min(...UFData.map(obs => parseFloat(obs.value)))"
+                         :y-axis-max="Math.max(...UFData.map(obs => parseFloat(obs.value)))" />
+            </ChartHolderCard>
+            </div>
+          <div class="col-lg-6 col-md-6 mt-6">
             <ChartHolderCard v-if="IPCData"
                              title="Valor del IPC"
                              subtitle="Últimos valores"
                              :update="getMinutesSinceLastUpdate() > 0 ? `${getMinutesSinceLastUpdate()} minutos` : 'Actualizado'"
                              color="secondary"
             >
-              <LineChart :chart="IPCData" id="line-chart-3" :yAxisMin="Math.min(...IPCData.datasets.data)"
-                         :y-axis-max="Math.max(...IPCData.datasets.data)"/>
+              <LineChart :data="IPCData" id="line-chart-5" :yAxisMin="Math.min(...IPCData.map(obs => parseFloat(obs.value)))"
+                         :y-axis-max="Math.max(...IPCData.map(obs => parseFloat(obs.value)))" />
             </ChartHolderCard>
           </div>
-          <div class="col-lg-6 col-md-6 mt-4">
+          <div class="col-lg-6 col-md-6 mt-6">
             <ChartHolderCard
-                title="Website Views"
-                subtitle="Last Campaign Performance"
-                :update="getMinutesSinceLastUpdate() > 0 ? `${getMinutesSinceLastUpdate()} minutos` : 'Actualizado'"
-                color="happLight"
+              title="Website Views"
+              subtitle="Last Campaign Performance"
+              :update="getMinutesSinceLastUpdate() > 0 ? `${getMinutesSinceLastUpdate()} minutos` : 'Actualizado'"
+              color="happLight"
             >
               <BarChart
-                  :chart="{
+                :chart="{
                               xAxislDatas: ['L', 'M', 'M', 'J', 'V', 'S', 'D'],
                               datasets: {
                                 label: 'Sales',
@@ -74,136 +73,42 @@
 </template>
 
 <script setup>
-import {onBeforeMount, onMounted, ref} from 'vue'
+import { onBeforeMount, ref } from 'vue'
 import axios from 'axios'
-import MiniStatisticsCard from '@/views/components/MiniStatisticsCard/MiniStatisticsCard.vue'
 import LineChart from '@/examples/Charts/LineChart.vue'
 import ChartHolderCard from '@/views/components/ChartHolderCard.vue'
 import BarChart from '@/examples/Charts/BarChart.vue'
 
 
-let MiniStatisticsData = ref([])
-let DolarData = ref(null)
-let UFData = ref(null)
 let UTMData = ref(null)
+let DOLARData = ref(null)
+let UFData = ref(null)
 let IPCData = ref(null)
-let previousData = ref(null)
 let lastUpdated = new Date()
 
 
+async function fetchData(url, dataRef) {
+  const response = await axios.get(url, {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET',
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Content-Type': 'application/json'
+    }
+  })
+  dataRef.value = response.data.Series.Obs.filter(obs => obs.value !== "NaN")
+}
+
 onBeforeMount(async () => {
-  const response = await axios.get('/api')
-  const data = response.data
-
-  DolarData.value = await getChartData('/api/dolar', 'Valor del Dólar')
-  UFData.value = await getChartData('/api/uf', 'Valor del UF')
-  IPCData.value = await getChartData('/api/ipc', 'Valor del IPC')
-  UTMData.value = await getChartData('/api/utm', 'Valor del UTM')
-  MiniStatisticsData.value = createStatisticsData(data)
+  await fetchData('https://localhost:8443/utm', UTMData)
+  await fetchData('https://localhost:8443/dolar', DOLARData)
+  await fetchData('https://localhost:8443/uf', UFData)
+  await fetchData('https://localhost:8443/ipc', IPCData)
 })
-
 function getMinutesSinceLastUpdate() {
   let now = new Date()
   let difference = now - lastUpdated
   return Math.floor(difference / 1000 / 60)
 }
-
-async function getChartData(apiEndpoint, label) {
-  const response = await axios.get(apiEndpoint)
-  const data = response.data.serie
-  lastUpdated = new Date()
-  return {
-    xAxislDatas: data.map((item) => new Date(item.fecha).toLocaleDateString('es-CL', {
-      year: 'numeric',
-      month: 'short',
-      day: 'numeric'
-    })).reverse(),
-    datasets: {
-      label: label,
-      data: data.map((item) => item.valor).reverse()
-    }
-  }
-}
-
-function createStatisticsData(data) {
-  const newData = [
-    {
-      'title': {
-        'text': 'Dólar',
-        'value': `$${data.dolar.valor}`
-      },
-      'detail': getDetail(DolarData, 'Dólar'),
-      'icon': {
-        'name': 'attach_money',
-        'color': 'white',
-        'background': 'happDark'
-      }
-    },
-    {
-      'title': {
-        'text': 'UF',
-        'value': `$${data.uf.valor}`
-      },
-      'detail': getDetail(UFData, 'UF'),
-      'icon': {
-        'name': 'UF',
-        'color': 'white',
-        'background': 'happDark'
-      }
-    },
-    {
-      'title': {
-        'text': 'UTM',
-        'value': `$${data.utm.valor}`
-      },
-      'detail': getDetail(UTMData, 'UTM'),
-      'icon': {
-        'name': 'UTM',
-        'color': 'white',
-        'background': 'dark'
-      }
-    },
-    {
-      'title': {
-        'text': 'IPC',
-        'value': `%${data.ipc.valor}`
-      },
-      'detail': getDetail(IPCData, 'IPC'),
-      'icon': {
-        'name': 'euro_symbol',
-        'color': 'white',
-        'background': 'secondary'
-      }
-    }
-  ]
-
-  // Almacenar los nuevos valores para la próxima comparación
-  previousData.value = newData
-
-  return newData
-}
-
-function getDetail(data, label) {
-  if (!data.value || !data.value.datasets.data.length) {
-    return '<span class=\'text-success text-sm font-weight-bolder\'> Actualizado</span>'
-  }
-
-  const lastValue = data.value.datasets.data[data.value.datasets.data.length - 1]
-  const secondLastValue = data.value.datasets.data[data.value.datasets.data.length - 2]
-
-  if (secondLastValue) {
-    let change
-    if (label === 'IPC') {
-      change = lastValue - secondLastValue
-      return `<span class='${change >= 0 ? 'text-success' : 'text-danger'} text-sm font-weight-bolder'> ${change.toFixed(2)} </span> ${change >= 0 ? 'más' : 'menos'} que el mes pasado`
-    } else {
-      change = ((lastValue - secondLastValue) / Math.abs(secondLastValue)) * 100
-      return `<span class='${change >= 0 ? 'text-success' : 'text-danger'} text-sm font-weight-bolder'> ${change.toFixed(2)}% </span> ${change >= 0 ? 'más' : 'menos'} que ayer`
-    }
-  } else {
-    return '<span class=\'text-success text-sm font-weight-bolder\'> Actualizado</span>'
-  }
-}
-
 
 </script>
